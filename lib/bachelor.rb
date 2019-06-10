62ae4f0a4835e52ff35c33ea300b2d18333738ec
Varvara Babkova <varvara.munday@gmail.com>
2019-06-10 19:14:52 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -56,7 +56,7 @@ def get_average_age_for_season(data, season)
       s_data.each do |person|
 
           cont_amount += 1
-          total_age += person["age"].to_i
+          total_age += person["age"].to_f
       end
 
     end
